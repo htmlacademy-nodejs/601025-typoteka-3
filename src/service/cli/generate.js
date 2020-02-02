7c8d2a43ea605d08e8e86e65e7b4b392c36624e3
Inspel <bozhan89@gmail.com>
2020-02-02 11:12:51 +0300

    10. Личный проект: асинхронность. Побег из ада коллбеков

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,5 +1,6 @@
 'use strict';
-const fs = require('fs');
+const fs = require('fs').promises;
+const chalk = require('chalk');
 const {
   ExitCode
 } = require('../../constants');
@@ -91,16 +92,15 @@ const generatePublications = (count) => {
 
 module.exports = {
   name: '--generate',
-  run(args) {
+  async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generatePublications(countOffer));
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error('Can\'t write data to file...');
-      }
-
-      return console.info('Operation success. File created.');
-    });
+    try {
+      await fs.writeFile(FILE_NAME, content);
+      console.error(chalk.green('Operation success. File created.'));
+    } catch (err) {
+      console.error(chalk.red('Can\'t write data to file...'));
+    }
   }
 };
