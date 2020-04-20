9e24b11e336b6e2a3619e73ad7090c285dddee4a
ABozhanov <bozhan89@gmail.com>
2020-04-20 12:43:20 +0300

    16. Личный проект: шаблонизируй это

--- src/express/routes/homeRoute.js
+++ src/express/routes/homeRoute.js
@@ -3,6 +3,8 @@
 const {Router} = require(`express`);
 const homeRoute = new Router();
 
-homeRoute.get(`/`, (req, res) => res.send(`/`));
+homeRoute.get(`/`, (req, res) => res.render('pages/main.pug'));
+homeRoute.get(`/500`, (req, res) => res.render('pages/errors/500.pug'));
+homeRoute.get(`/400`, (req, res) => res.render('pages/errors/400.pug'));
 
 module.exports = homeRoute;
